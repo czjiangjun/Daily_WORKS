a3e031de9730a4aad01a328a6773e91bf75f2ca3
Jun-Jiang <czjiangjun@gmail.com>
2021-12-23 22:25:41 +0800

    update

--- Creat_model_binding_File.py
+++ Creat_model_binding_File.py
@@ -283,42 +283,74 @@ locate_path = os.getcwd()
 # object_path = ['6-p05', '6-p12', '6-p18', '9-p05', '9-p10', '9-p11', '9-p12', '9-p13', '9-p18']
 #object_path = ['6-p02', '6-p05_2', '6-p08', '6-p10', '6-p11', '6-p12_2', '6-p18_2', '6-p19', '9-p02', '9-p08', '9-p19']
 # object_path = ['6-p02_test']
+test_POSCAR = 1
 VASP_path = ['1-Relax_Step1','1-Relax_Step2','1-Relax_Step3','2-Static']
 
 POSCAR_replace_file = locate_path +'/'+ 'POSCAR'+'_'+object_path
 POSCAR_Alloy_file = locate_path +'/'+ Alloy + '/' +'2-Static' +'/' + 'POSCAR'
 if not(os.path.exists(POSCAR_Alloy_file)):
     POSCAR_Alloy_file = locate_path +'/'+ Alloy + '/' +'1-Relax_Step1' +'/' + 'POSCAR'
+    test_POSCAR = 0
 
 # print(POSCAR_Alloy_file)
 # exit(0)
 Replace_Element(replace_position, POSCAR_orig_path, POSCAR_Alloy_file, POSCAR_replace_file)
 # exit (0)
 
-# for j in object_path
-#    current_path = os.path.join(locate_path,Binding, j)
-current_path = os.path.join(locate_path, Binding, object_path)
+if (test_POSCAR == 0):
+    for j in VASP_path:
+         current_path = os.path.join(locate_path, object_path, j)
 #        current_path=locate_path+'/'+'Model-2021-09-06'+'/'+i+'/'+j
 #        print(current_path)
 #        exit()
-if not(os.path.exists(current_path)):
-    os.makedirs(current_path)
-os.chdir(current_path)
-shutil.copyfile(POSCAR_replace_file, 'POSCAR')
-elements = linecache.getline(POSCAR_replace_file, 6)
+         if not(os.path.exists(current_path)):
+             os.makedirs(current_path)
+         os.chdir(current_path)
+         if (j == VASP_path[0]):
+             shutil.copyfile(POSCAR_replace_file, 'POSCAR')
+             elements = linecache.getline(POSCAR_replace_file, 6)
 #        print(elements)
 #            exit()
-generate_potcar = 'pmg potcar -f PBE -s '+ (elements)
-os.system(generate_potcar)
+             generate_potcar = 'pmg potcar -f PBE -s '+ (elements)
+             os.system(generate_potcar)
 #            exit()
-INCAR_orig = os.path.join(locate_path, 'INCAR_2-Static')
+         else:
+             POTCAR_orig = '../1-Relax_Step1/POTCAR'
+             shutil.copyfile(POTCAR_orig, 'POTCAR')
+         INCAR_orig = os.path.join(locate_path, 'INCAR_'+j)
 #        INCAR_orig = locate_path + '/'+'INCAR_'+j
 #        print(INCAR_orig)
 #       exit()
-KPOINTS_orig = os.path.join(locate_path, 'KPOINTS_2-Static')
+         KPOINTS_orig = os.path.join(locate_path, 'KPOINTS_'+j)
+         shutil.copyfile(INCAR_orig, 'INCAR')
+         shutil.copyfile(KPOINTS_orig, 'KPOINTS')
+else:
+# for j in object_path
+#    current_path = os.path.join(locate_path,Binding, j)
+    current_path = os.path.join(locate_path, Binding, object_path)
+#        current_path=locate_path+'/'+'Model-2021-09-06'+'/'+i+'/'+j
+#        print(current_path)
+#        exit()
+    if not(os.path.exists(current_path)):
+        os.makedirs(current_path)
+    os.chdir(current_path)
+    shutil.copyfile(POSCAR_replace_file, 'POSCAR')
+    elements = linecache.getline(POSCAR_replace_file, 6)
+#        print(elements)
+#            exit()
+    generate_potcar = 'pmg potcar -f PBE -s '+ (elements)
+    os.system(generate_potcar)
+#            exit()
+    INCAR_orig = os.path.join(locate_path, 'INCAR_2-Static')
+#        INCAR_orig = locate_path + '/'+'INCAR_'+j
+#        print(INCAR_orig)
+#       exit()
+    KPOINTS_orig = os.path.join(locate_path, 'KPOINTS_2-Static')
 #        KPOINTS_orig = locate_path + '/'+'KPOINTS_'+j
-shutil.copyfile(INCAR_orig, 'INCAR')
-shutil.copyfile(KPOINTS_orig, 'KPOINTS')
+
+
+    shutil.copyfile(INCAR_orig, 'INCAR')
+    shutil.copyfile(KPOINTS_orig, 'KPOINTS')
         
 # current_path = os.path.join(locate_path, Binding)
 # os.chdir(current_path)
