febc18f73dc77540cd82ba5e3032eed5972d6be4
czjiangjun <czjiangjun@gmail.com>
2022-03-09 17:53:17 +0800

    2022-03-09_PythonGUI

--- WORKS_Scripts/Python_GUI/Python_easyGUI.py
+++ WORKS_Scripts/Python_GUI/Python_easyGUI.py
@@ -3,7 +3,7 @@ import easygui as a
 a.msgbox(msg='Today', title=' TEST ', ok_button=' Continue ', image=None, root=None)
 
 # ccbox(msg=' ', title=' ', choices=('  ', '  '), image=None)  # 双项选择
-a.ccbox(msg=' Today', title=' TEST ', choices=(' OK  ', ' Cancel '), image=None)  # 双项选择
+a.ccbox(msg=' Today', title=' TEST ', choices=(' OK  ', ' Cancel '), image='/home/jun_jiang/Documents/Latex_art_beamer/Presentation_Beamer/Figures/seal_Jiang-2.jpg')  # 双项选择
 
 # buttonbox(msg=' ', title=' ', choices=('Button1', 'Button2', 'Button3'), image=None, root=None) # 多项选择
 a.buttonbox(msg=' Today', title=' TEST', choices=('Button1', 'Button2', 'Button3'), image=None)
@@ -16,7 +16,27 @@ ret = a.multchoicebox(msg = 'Question:', title=' TEST', choices=['choice1','choi
 print(ret)
 
 # enterbox(msg='  ', title=' ', default=' ', strip=True, image=None, root=None) 文本输入诓
-ret = a.enterbox(msg=' Question ', title=' Dialog_box ', default=' TEST ', strip=True, image=None) # 文本输入诓
+ret = a.enterbox(msg=' Question ', title=' Dialog_box ', default=' TEST ', strip=True, image=None)
+print(ret)
+
+# integerbox(msg='', title=' ', default='', lowerbound=0, upperbound=99, image=None, root=None, **invalidKeywordArguments) # 只能输入界定范围内的整型数
+ret = a.integerbox(msg=' Question ', title=' Dialog_box', default=666, lowerbound=0, upperbound=999, image=None )
+print(ret)
+
+Msg = '上网不涉密，涉密不上网'
+Title = '校园网充值中心'
+Fields = ['姓名', '学号', '充值面额']
+# multenterbox(msg=' ', title=' ', fields=(), values=()) 其中values是输入的默认值、feilds是需要填写的条目名称，均用列表填写； 返回值是所有填写的值构成的列表；
+ret = a.multenterbox(msg = Msg, title = Title, fields= Fields, values= ['0', '0', '0'])
+print(ret)
+
+# passwordbox(msg=' ', title=' ', default=' ', image=None, root=None) # 密码输入框(不显示)
+ret = a.passwordbox(msg=' 输入密码 ', title=' 密码登录 ', default=' ', image=None, root=None)
+print(ret)
+
+User_and_Pass = ['用户名', '账号', '密码']
+# multpasswordbox() # 与上面类似，不过其只有最后一个框是匿名的，即密码输入
+ret = a.multpasswordbox(msg = Msg, title = Title, fields = User_and_Pass, values = ['', '', ''])
 print(ret)
 
 def main():
---
+++ WORKS_Scripts/Python_GUI/Python_wxPython_learn-4.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env ~/.local/bin/python3
+#-*- coding:utf-8 -*-
+import os
+import wx
+
+
+# app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
+# frame = wx.Frame(None, wx.ID_ANY, "Hello World") # A Frame is a top-level window.
+# frame.Show(True)     # Show the frame.
+# app.MainLoop()
+
+# class MyFrame(wx.Frame):
+#     """ We simply derive a new class of Frame. """
+#     def __init__(self, parent, title):
+#         wx.Frame.__init__(self, parent, title=title, size=(200,100))
+#         self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+#         self.Show(True)
+
+# app = wx.App(False)
+# frame = MyFrame(None, 'Small editor')
+# app.MainLoop()
+
+# class MainWindow(wx.Frame):
+#     def __init__(self, parent, title):
+#         wx.Frame.__init__(self, parent, title=title, size=(200,100))
+#         self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+#         self.CreateStatusBar() # A Statusbar in the bottom of the window
+
+        # Setting up the menu.
+#         filemenu= wx.Menu()
+
+        # wx.ID_ABOUT and wx.ID_EXIT are standard IDs provided by wxWidgets.
+#         filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
+#         filemenu.AppendSeparator()
+#         filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
+
+        # Creating the menubar.
+#         menuBar = wx.MenuBar()
+#         menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
+#         self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+#         self.Show(True)
+
+# app = wx.App(False)
+# frame = MainWindow(None, "Sample editor")
+# app.MainLoop()
+
+
+class MainWindow(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(200,100))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar() # A StatusBar in the bottom of the window
+
+        # Setting up the menu.
+        filemenu= wx.Menu()
+#        filemenu2= wx.Menu()
+
+        # wx.ID_ABOUT and wx.ID_EXIT are standard ids provided by wxWidgets.
+        menuAbout = filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
+        menuExit = filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
+
+        # Creating the menubar.
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
+#        menuBar.Append(filemenu2,"&TEST") # Adding the "filemenu" to the MenuBar
+        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+
+        # Set events.
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+
+        self.Show(True)
+
+    def OnAbout(self,e):
+        # A message dialog box with an OK button. wx.OK is a standard ID in wxWidgets.
+        dlg = wx.MessageDialog( self, "A small text editor", "About Sample Editor", wx.OK)
+        dlg.ShowModal() # Show it
+        dlg.Destroy() # finally destroy it when finished.
+
+    def OnExit(self,e):
+        self.Close(True)  # Close the frame.
+
+    def OnOpen(self,e):
+        """ Open a file"""
+        self.dirname = ''
+        dlg = wx.FileDialog(self, "Choose a file", self.dirname, "", "*.*", wx.FD_OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.filename = dlg.GetFilename()
+            self.dirname = dlg.GetDirectory()
+            f = open(os.path.join(self.dirname, self.filename), 'r')
+            self.control.SetValue(f.read())
+            f.close()
+        dlg.Destroy()
+
+app = wx.App(False)
+frame = MainWindow(None, "Sample editor")
+app.MainLoop()
